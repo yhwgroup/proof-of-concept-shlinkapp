477874e2f7b8558a902f740412060592e386f01c
吳元皓 <hpware3@gmail.com>
2025-02-19 00:26:37 +0800

    this is today's work.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { Text, View } from "react-native";
 import * as shl from "@shlinkio/shlink-js-sdk";
-import { FetchHttpClient } from "@shlinkio/shlink-js-sdk/fetch";
+import * as shlf from "@shlinkio/shlink-js-sdk/fetch";
 
 const sinfo = {
   baseUrl: "https://go.yuanhau.com",
   apiKey: process.env.shlink_api,
 }
-
+export const shlink = new shl.ShlinkApiClient(
+  new shlf.FetchHttpClient(),
+  sinfo
+)
 export default function Index() {
   return (
     <View
