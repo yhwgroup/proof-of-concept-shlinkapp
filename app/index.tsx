b72b3dfae25251138cdec8c2b177f6105f61e4e7
吳元皓 <hpware3@gmail.com>
2025-02-25 08:52:02 +0800

    Fix import statement for FetchHttpClient in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
 import { Text, View } from "react-native";
 import * as shl from "@shlinkio/shlink-js-sdk";
-import * as shlf from "@shlinkio/shlink-js-sdk/fetch";
 
+ 
 const sinfo = {
   baseUrl: "https://go.yuanhau.com",
   apiKey: process.env.shlink_api,
 }
 export const shlink = new shl.ShlinkApiClient(
-  new shlf.FetchHttpClient(),
+  new FetchHttpClient(),
   sinfo
 )
 export default function Index() {
